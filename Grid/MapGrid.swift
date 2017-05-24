3d7c640274cf063f4d62e268603e29b3c5569cf0
Mikko Välimäki <mkko1373@gmail.com>
2017-05-24 23:22:46 +0200

    MapGrid implementation

--- Grid/MapGrid.swift
+++ Grid/MapGrid.swift
@@ -9,20 +9,21 @@
 import Foundation
 import MapKit
 
-public struct MapTile {
+public struct MapTile<T> {
     public let mapIndex: MapIndex
+    public let item: T
 }
 
 extension MapTile: CustomDebugStringConvertible {
     
     public var debugDescription: String {
-        return "(x:\(self.mapIndex.index.x), y:\(self.mapIndex.index.y))"
+        return "(x:\(self.mapIndex.index.x), y:\(self.mapIndex.index.y), item: \(item))"
     }
 }
 
-public struct TileUpdate {
-    public let newTiles: [MapTile]
-    public let removedTiles: [MapTile]
+public struct TileUpdate<T> {
+    public let newTiles: [MapTile<T>]
+    public let removedTiles: [MapTile<T>]
 }
 
 public struct MapIndex {
@@ -36,38 +37,55 @@ extension MapIndex: Equatable {
     }
 }
 
-public class MapGrid {
+public class MapGrid<T> {
     
-    var grid: Grid<MapTile>
+    var grid: Grid<MapTile<T>>
     
     let tileLatitudinalSize: CLLocationDistance
     
     let tileLongitudinalSize: CLLocationDistance
     
-    public convenience init(tileSize: CLLocationDistance) {
-        self.init(tileLatitudinalSize: tileSize, tileLongitudinalSize: tileSize)
+    // Protocols don't support generics the way it is needed here, so we use base class.
+    private let factory: TileFactory<T>
+    
+    public var visibleTiles: [MapTile<T>] {
+        return grid.getTiles().map { $0.1 }
     }
     
-    public init(tileLatitudinalSize: CLLocationDistance, tileLongitudinalSize: CLLocationDistance) {
+    public convenience init(tileSize: CLLocationDistance, factory: TileFactory<T>) {
+        self.init(tileLatitudinalSize: tileSize, tileLongitudinalSize: tileSize, factory: factory)
+    }
+    
+    public init(tileLatitudinalSize: CLLocationDistance, tileLongitudinalSize: CLLocationDistance, factory: TileFactory<T>) {
         self.tileLatitudinalSize = tileLatitudinalSize
         self.tileLongitudinalSize = tileLongitudinalSize
         
-        grid = Grid<MapTile>()
+        self.grid = Grid<MapTile<T>>()
+        self.factory = factory
     }
-    public func update(visibleRegion region: MKCoordinateRegion) -> TileUpdate {
+    
+    public func update(visibleRegion region: MKCoordinateRegion) -> TileUpdate<T> {
         let gridIndices = getGridRect(forRegion: region, withOrigin: self.regionOfOrigin)
         return update(visibleIndices: gridIndices)
+    }
+    
+    public func region(at mapIndex: MapIndex) -> MKCoordinateRegion {
+        return MKCoordinateRegion(
+            center: CLLocationCoordinate2D(
+                latitude: self.regionOfOrigin.span.latitudeDelta * (Double(mapIndex.index.y)+0.5),
+                longitude: self.regionOfOrigin.span.longitudeDelta * (Double(mapIndex.index.x)+0.5)),
+            span: self.regionOfOrigin.span)
     }
     
     /**
      Load tiles for the given indices. Returns the delta.
      */
-    public func update(visibleIndices: [MapIndex]) -> TileUpdate {
+    private func update(visibleIndices: [MapIndex]) -> TileUpdate<T> {
         
         // TODO: Could be improved in performance.
         var visibleGridIndices = visibleIndices.map { $0.index }
-        var existingTiles = [MapTile]()
-        var removedTiles = [MapTile]()
+        var existingTiles = [MapTile<T>]()
+        var removedTiles = [MapTile<T>]()
         
         // Check the existing ones first.
         for (index, mapTile) in grid.getTiles() {
@@ -83,17 +101,20 @@ public class MapGrid {
         }
         
         // Remaining visible indices should be new.
-        var newTiles = [MapTile]()
+        var newTiles = [MapTile<T>]()
         for gridIndex in visibleGridIndices {
-            let tile = MapTile(mapIndex: MapIndex(index: gridIndex))
+            let mapIndex = MapIndex(index: gridIndex)
+            // FIXME: Don't cast.
+            let item: T? = self.factory.value(forMapIndex: mapIndex, inMapGrid: self)
+            let tile = MapTile(mapIndex: mapIndex, item: item!)
             newTiles.append(tile)
             grid[gridIndex] = tile
         }
         
-        print("new: \(newTiles)")
-        print("removed: \(removedTiles)")
+//        print("new: \(newTiles)")
+//        print("removed: \(removedTiles)")
         
-        return TileUpdate(newTiles: newTiles.map, removedTiles: removedTiles)
+        return TileUpdate(newTiles: newTiles, removedTiles: removedTiles)
     }
     
     var regionOfOrigin: MKCoordinateRegion {
@@ -106,15 +127,15 @@ public class MapGrid {
 fileprivate func getGridRect(forRegion region: MKCoordinateRegion, withOrigin origin: MKCoordinateRegion) -> [MapIndex] {
     
     let north = (region.center.latitude + region.span.latitudeDelta / 2.0) - origin.center.latitude
-    let east = (region.center.longitude + region.span.longitudeDelta / 2.0) - origin.center.longitude
     let south = (region.center.latitude - region.span.latitudeDelta / 2.0) - origin.center.latitude
+    let east = (region.center.longitude + region.span.longitudeDelta / 2.0) - origin.center.longitude
     let west = (region.center.longitude - region.span.longitudeDelta / 2.0) - origin.center.longitude
     
     let y1 = Int(floor(south / origin.span.latitudeDelta))
-    let y2 = Int(ceil(north / origin.span.latitudeDelta))
+    let y2 = Int(floor(north / origin.span.latitudeDelta))
     
     let x1 = Int(floor(west / origin.span.longitudeDelta))
-    let x2 = Int(ceil(east / origin.span.longitudeDelta))
+    let x2 = Int(floor(east / origin.span.longitudeDelta))
     
     
     return (x1...x2).flatMap { x in
